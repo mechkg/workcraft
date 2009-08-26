d3e899791f4acc982e98db8bd05e6cdafb0629c2
Ivan Poliakov <ivpolyakov@gmail.com>
2009-08-26 17:34:14 +0000

    Routine commit

--- workcraft/plugins/serialisation/xml/EnumSerialiser.java
+++ workcraft/plugins/serialisation/xml/EnumSerialiser.java
@@ -4,13 +4,14 @@ import org.w3c.dom.Element;
 import org.workcraft.framework.exceptions.SerialisationException;
 import org.workcraft.framework.serialisation.xml.BasicXMLSerialiser;
 
-public class DoubleSerialiser implements BasicXMLSerialiser{
-	public String getClassName() {
-		return double.class.getName();
-	}
-
+public class EnumSerialiser implements BasicXMLSerialiser {
 	public void serialise(Element element, Object object)
 			throws SerialisationException {
-		element.setAttribute("value", ((Double)object).toString());
+		element.setAttribute("value", object.toString());
+
+	}
+
+	public String getClassName() {
+		return Enum.class.getName();
 	}
 }
--- workcraft/plugins/serialisation/xml/IntSerialiser.java
+++ workcraft/plugins/serialisation/xml/IntSerialiser.java
@@ -4,13 +4,13 @@ import org.w3c.dom.Element;
 import org.workcraft.framework.exceptions.SerialisationException;
 import org.workcraft.framework.serialisation.xml.BasicXMLSerialiser;
 
-public class DoubleSerialiser implements BasicXMLSerialiser{
+public class IntSerialiser implements BasicXMLSerialiser{
 	public String getClassName() {
-		return double.class.getName();
+		return int.class.getName();
 	}
 
 	public void serialise(Element element, Object object)
 			throws SerialisationException {
-		element.setAttribute("value", ((Double)object).toString());
+		element.setAttribute("value", ((Integer)object).toString());
 	}
 }

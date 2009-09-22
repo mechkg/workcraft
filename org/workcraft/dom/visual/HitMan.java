18bc35ee899c6ba507d795d842b8dacdc266a940
Ivan Poliakov <ivpolyakov@gmail.com>
2009-09-22 20:58:22 +0100

    Added support for Hidable nodes (they will not be hit in hit detection)

--- org/workcraft/dom/visual/HitMan.java
+++ org/workcraft/dom/visual/HitMan.java
@@ -4,7 +4,9 @@ import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 import net.sf.jga.algorithms.Filter;
 import net.sf.jga.fn.UnaryFunctor;
@@ -42,7 +44,7 @@ public class HitMan
 	{
 		return reverse(filterByBB(node.getChildren(), point));
 	}
-	
+
 	public static Node hitDeepest(Point2D point, Node node, UnaryFunctor<Node, Boolean> filter) {
 		Point2D transformedPoint = transformToChildSpace(point, node);
 
@@ -60,11 +62,15 @@ public class HitMan
 
 	public static boolean hitBranch (Point2D point, Node node) {
 
-		if (node instanceof Touchable && ((Touchable)node).hitTest(point))
-			return true;
+		if (node instanceof Touchable && ((Touchable)node).hitTest(point))	{
+			if (node instanceof Hidable)
+				return !((Hidable)node).isHidden();
+			else
+				return true;
+		}
 
 		Point2D transformedPoint = transformToChildSpace(point, node);
-		
+
 		for (Node n : getFilteredChildren(transformedPoint, node)) {
 			if (hitBranch(transformedPoint, n))
 				return true;
@@ -81,7 +87,7 @@ public class HitMan
 			}
 		});
 	}
-	
+
 	public static Node hitFirst(Point2D point, Node node, UnaryFunctor<Node, Boolean> filter) {
 		if (filter.fn(node)) {
 			if (hitBranch(point, node))
@@ -92,7 +98,7 @@ public class HitMan
 			return hitFirstChild(point, node, filter);
 		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public static <T extends Node> T hitFirstNodeOfType(Point2D point, Node node, Class<T> type) {
 		return (T) hitFirst(point, node, Hierarchy.getTypeFilter(type));
@@ -108,12 +114,12 @@ public class HitMan
 		}
 		return null;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public static <T extends Node> T hitFirstChildOfType(Point2D point, Node node, Class<T> type) {
 		return (T) hitFirstChild(point, node, Hierarchy.getTypeFilter(type));
 	}
-	
+
 	private static Point2D transformToChildSpace(Point2D point,
 			Node node) {
 		Point2D transformedPoint; 
@@ -126,8 +132,8 @@ public class HitMan
 			transformedPoint = point;
 		return transformedPoint;
 	}
-	
-	
+
+
 	private static <T> Iterable<T> reverse(Iterable<T> original)
 	{
 		final ArrayList<T> list = new ArrayList<T>(); 
@@ -164,8 +170,11 @@ public class HitMan
 	public static Node hitTestForSelection(Point2D point, Node node) {
 		Node nd = HitMan.hitFirstChild(point, node, new UnaryFunctor<Node, Boolean>() {
 			public Boolean fn(Node n) {
-				if (n instanceof Movable)
-					return true;
+				if (!(n instanceof Movable))
+					return false;
+
+				if (n instanceof Hidable)
+					return !((Hidable)n).isHidden();
 				else
 					return false;
 			}
@@ -174,8 +183,12 @@ public class HitMan
 		if (nd == null)
 			nd = HitMan.hitFirstChild(point, node, new UnaryFunctor<Node, Boolean>() {
 				public Boolean fn(Node n) {
-					if (n instanceof VisualConnection)
-						return true;
+					if (n instanceof VisualConnection) {
+						if (n instanceof Hidable) 
+							return !((Hidable)n).isHidden();
+						else
+							return true;
+					}
 					else
 						return false;
 				}
@@ -183,13 +196,17 @@ public class HitMan
 
 		return nd;
 	}
-	
+
 	@SuppressWarnings("serial")
 	public static Node hitTestForConnection(Point2D point, Node node) {
 		Node nd = HitMan.hitDeepest(point, node, new UnaryFunctor<Node, Boolean>() {
 			public Boolean fn(Node n) {
-				if (n instanceof Movable && ! (n instanceof Container))
-					return true;
+				if (n instanceof Movable && ! (n instanceof Container)) {
+					if (n instanceof Hidable) 
+						return !((Hidable)n).isHidden();
+					else
+						return true;					
+				}
 				else
 					return false;
 			}
@@ -198,8 +215,12 @@ public class HitMan
 		if (nd == null)
 			nd = HitMan.hitDeepest(point, node, new UnaryFunctor<Node, Boolean>() {
 				public Boolean fn(Node n) {
-					if (n instanceof VisualConnection)
-						return true;
+					if (n instanceof VisualConnection) {
+						if (n instanceof Hidable) 
+							return !((Hidable)n).isHidden();
+						else
+							return true;
+					}
 					else
 						return false;
 				}
@@ -207,18 +228,47 @@ public class HitMan
 
 		return nd;
 	}
-	
+
 	public static Node hitTestForConnection (Point2D point, VisualModel model) {
 		AffineTransform t = TransformHelper.getTransform(model.getRoot(), model.getCurrentLevel());
 		Point2D pt = new Point2D.Double();
 		t.transform(point, pt);
 		return hitTestForConnection(pt, model.getCurrentLevel());	
 	}
-	
+
 	public static Node hitTestForSelection (Point2D point, VisualModel model) {
 		AffineTransform t = TransformHelper.getTransform(model.getRoot(), model.getCurrentLevel());
 		Point2D pt = new Point2D.Double();
 		t.transform(point, pt);
 		return hitTestForSelection(pt, model.getCurrentLevel());	
+	}
+
+	public static Collection<Node> boxHitTest (VisualGroup group, Point2D p1, Point2D p2) {
+		Point2D p1local = new Point2D.Double();
+		Point2D p2local = new Point2D.Double();
+		group.getParentToLocalTransform().transform(p1, p1local);
+		group.getParentToLocalTransform().transform(p2, p2local);
+
+		LinkedList<Node> hit = new LinkedList<Node>();
+
+		Rectangle2D rect = new Rectangle2D.Double(
+				Math.min(p1local.getX(), p2local.getX()), 
+				Math.min(p1local.getY(), p2local.getY()), 
+				Math.abs(p1local.getX()-p2local.getX()), 
+				Math.abs(p1local.getY()-p2local.getY()));
+
+		for (Touchable n : Hierarchy.getChildrenOfType(group, Touchable.class)) {
+			if (n instanceof Hidable && ((Hidable)n).isHidden() )
+				continue;
+
+			if (p1local.getX()<=p2local.getX()) {
+				if (TouchableHelper.insideRectangle(n, rect))
+					hit.add((VisualNode)n);
+			} else {
+				if (TouchableHelper.touchesRectangle(n, rect))
+					hit.add((VisualNode)n);
+			}
+		}
+		return hit;
 	}
 }

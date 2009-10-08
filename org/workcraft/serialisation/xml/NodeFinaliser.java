36828d257c251cae210c8de6ee73cb99eb150d3a
Ivan Poliakov <ivpolyakov@gmail.com>
2009-10-08 19:46:25 +0100

    Fixed serialisation issues:
    * Separated createInstance from initInstance in CustomXMLDeserialiser
    
    * initIsntance is now called for all classes in the hierarchy to allow initialisation of required child nodes
    
    * Cleaned interfaces

--- org/workcraft/serialisation/xml/NodeFinaliser.java
+++ org/workcraft/serialisation/xml/NodeFinaliser.java
@@ -21,14 +21,8 @@
 
 package org.workcraft.serialisation.xml;
 
-import org.w3c.dom.Element;
 import org.workcraft.exceptions.DeserialisationException;
-import org.workcraft.serialisation.ReferenceResolver;
 
-public interface ReferencingXMLDeserialiser extends XMLDeserialiser {
-	public void deserialise(Element element, Object instance,
-			ReferenceResolver internalReferenceResolver,
-			ReferenceResolver externalReferenceResolver
-			)
-			throws DeserialisationException;
+public interface NodeFinaliser {
+	public void finaliseInstance(Object instance) throws DeserialisationException;
 }

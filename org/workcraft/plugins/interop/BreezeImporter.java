f8befa3330e002710659e33e431fc79c4f3497e8
Ivan Poliakov <ivpolyakov@gmail.com>
2009-11-25 11:36:20 +0000

    Started implementation of the Breeze file parser

--- org/workcraft/plugins/interop/BreezeImporter.java
+++ org/workcraft/plugins/interop/BreezeImporter.java
@@ -24,11 +24,11 @@ package org.workcraft.plugins.interop;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 
 import org.workcraft.dom.Model;
 import org.workcraft.exceptions.DeserialisationException;
 import org.workcraft.interop.Importer;
+import org.workcraft.parsers.lisp.LispNode;
 import org.workcraft.parsers.lisp.LispParser;
 import org.workcraft.parsers.lisp.ParseException;
 
@@ -50,8 +50,8 @@ public class BreezeImporter implements Importer {
 			IOException {
 		
 		try {
-			List<Object> list = LispParser.parse(in);
-			System.out.println(list);			
+			LispNode list = LispParser.parse(in);
+			System.out.println(list.getSubNodes());			
 		} catch (ParseException e) {
 			throw new DeserialisationException(e);
 		}

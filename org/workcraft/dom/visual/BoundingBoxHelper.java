f0c6ec0843892fd4e022adb21ecd14598f3f1528
Andrey Mokhov <andrey.mokhov@ncl.ac.uk>
2010-04-08 16:51:10 +0100

    CPOG model implementation.

--- org/workcraft/dom/visual/BoundingBoxHelper.java
+++ org/workcraft/dom/visual/BoundingBoxHelper.java
@@ -45,6 +45,20 @@ public class BoundingBoxHelper {
 			bb = union(bb, node.getBoundingBox());
 		return bb;
 	}
+	
+	public static Rectangle2D expand(Rectangle2D rect, double x, double y)
+	{
+		Rectangle2D res = new Rectangle2D.Double();
+		res.setRect(rect);
+		
+		x /= 2.0f;
+		y /= 2.0f;
+		
+		res.add(rect.getMinX() - x, rect.getMinY() - y);
+		res.add(rect.getMaxX() + x, rect.getMaxY() + y);
+
+		return res;
+	}
 
 	
 }

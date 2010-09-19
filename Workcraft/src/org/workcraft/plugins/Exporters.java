8cf2ddc2bb08d5ad093849e240951cd198618b62
Ivan Poliakov <ivpolyakov@gmail.com>
2010-09-19 02:46:02 +0100

    - Balsa plugin refactoring: converted to using the asynchronous tasks framework, export tools are now Tools instead of Exporters. - Generalised the tool preset dialog and applied it to "Control STG Extraction" configuration.

--- Workcraft/src/org/workcraft/plugins/Exporters.java
+++ Workcraft/src/org/workcraft/plugins/Exporters.java
@@ -5,6 +5,7 @@ import org.workcraft.Initialiser;
 import org.workcraft.Module;
 import org.workcraft.PluginManager;
 import org.workcraft.interop.Exporter;
+import org.workcraft.interop.Importer;
 import org.workcraft.plugins.interop.DotExporter;
 import org.workcraft.plugins.interop.DotGExporter;
 import org.workcraft.plugins.interop.DotGImporter;
@@ -16,10 +17,6 @@ public class Exporters implements Module {
 	@Override
 	public Class<?>[] getPluginClasses() {
 		return new Class<?>[]{
-				DotExporter.class,
-				DotGExporter.class,
-				SVGExporter.class,
-				DotGImporter.class
 		};
 	}
 
@@ -33,6 +30,11 @@ public class Exporters implements Module {
 			}
 		});
 		
+		p.registerClass(Exporter.class, DotExporter.class);
+		p.registerClass(Exporter.class, DotGExporter.class);
+		p.registerClass(Exporter.class, SVGExporter.class);
+		p.registerClass(Importer.class, DotGImporter.class);
+
 	}
 
 }

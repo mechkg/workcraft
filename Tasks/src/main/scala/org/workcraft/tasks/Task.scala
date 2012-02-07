4ea0c299cc8e6c64aa9fb21701b6c4284ae6ae64
Ivan Poliakov <ivpolyakov@gmail.com>
2012-02-07 17:03:23 +0000

    Some work on Tasks

--- Tasks/src/main/scala/org/workcraft/tasks/Task.scala
+++ Tasks/src/main/scala/org/workcraft/tasks/Task.scala
@@ -1,13 +1,41 @@
 package org.workcraft.tasks
+import scalaz.effects.IO
 
-sealed trait Progress[T]
+import org.workcraft.scala.Expressions._
 
-object Progress {
-  case class KnownProgress[T] (val progress: Double) extends Progress[T]
-  case class UnknownProgress[T] extends Progress[T]
-  case class Completed[T] (val result: T) extends Progress[T]
-}
+sealed trait Progress[O, E]
 
-trait Task[T] {
-  def run : IO[Expression[Progress]] 
+case class KnownProgress (val progress: Double) extends Progress[Any, Any]
+case class UnknownProgress extends Progress[Any, Any]
+case class Completed[O, E] (val result: Outcome[O, E]) extends Progress[O, E]
+
+sealed trait Outcome[O, E]
+
+case class Failed[E](val error: E) extends Outcome[Any,E]
+case class Cancelled extends Outcome[Any, Any]
+case class Finished[O](val output: O) extends Outcome[O, Any]
+ 
+
+trait Task[I, O, E] {
+  def description : Expression[String]
+  def action (input: I) : IO[Expression[Progress[O, E]]]
+ 
+  def eval[T](e:Expression[T]) : T
+  
+  def start (input: I): Outcome[O, E] = {
+    
+  }
+  
+  def run (input: I) : Outcome [O, E] = {
+      val p = action(input).unsafePerformIO
+      
+      while (true) {
+        eval(p) match {
+          case Completed(outcome) => return outcome
+          case _ => Thread.sleep(30)
+       }
+      }
+      
+      throw new RuntimeException("stfu")
+  }
 }

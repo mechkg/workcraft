09987b3c28ad8382c6bc8d8cb573ae370a473c13
Ivan Poliakov <ivpolyakov@gmail.com>
2012-04-25 13:57:35 +0100

    Deleted unneeded files
    Removed some unneeded files that were deleted before but re-appeared following migration from Bazaar and broke the build.

--- Graphics/src/main/scala/org/workcraft/graphics/formularendering/PodgonFontRenderContext.scala
+++
@@ -1,8 +0,0 @@
-package org.workcraft.graphics.formularendering
-import java.awt.font.FontRenderContext
-import java.awt.geom.AffineTransform
-
-object PodgonFontRenderContext {
-	lazy val instance = new FontRenderContext(AffineTransform.getScaleInstance(1000, 1000), true, true)
-	def apply = instance
-}
